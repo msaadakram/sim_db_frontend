0c2c0960143bd09dc372647563881e5690f664a5
msaadakram <arhamsaad453@gmail.com>
2026-04-04 09:21:33 +0500

    chore(search): remove head ad script and add ads.txt

--- src/app/search/head.tsx
+++
@@ -1,5 +0,0 @@
-export default function Head() {
-    return (
-        <script src="https://pl29023950.profitablecpmratenetwork.com/e9/16/bc/e916bcc84635e25aa4cd4b692f26a06c.js"></script>
-    );
-}
