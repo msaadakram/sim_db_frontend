856fa3919e71195c648eac58dec050eb0587d9f9
msaadakram <arhamsaad453@gmail.com>
2026-04-04 08:49:45 +0500

    feat(search): add profitablecpm script in search page head

---
+++ src/app/search/head.tsx
@@ -0,0 +1,5 @@
+export default function Head() {
+    return (
+        <script src="https://pl29023950.profitablecpmratenetwork.com/e9/16/bc/e916bcc84635e25aa4cd4b692f26a06c.js"></script>
+    );
+}
